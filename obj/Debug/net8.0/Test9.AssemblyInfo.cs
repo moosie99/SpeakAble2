8460b861ac4d92cd41277e9be47c18a672aafd3d
N0987943 <emma.harris2021@my.ntu.ac.uk>
2024-04-18 11:52:37 +0100

    Commit made from VS Code Azure Static Web Apps

--- obj/Debug/net8.0/Test9.AssemblyInfo.cs
+++ obj/Debug/net8.0/Test9.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9685e5345e658846696d81435f235494f129399b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef23f091007645728040545402ba9f227ac0408c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
